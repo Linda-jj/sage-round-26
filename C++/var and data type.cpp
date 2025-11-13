75c5d72e9722515dfc2e24c7dc0b0a95c0048044
lideya <lideyafff@gmail.com>
2025-11-13 12:24:30 +0300

    var and data type

---
+++ C++/var and data type.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int age=33;
+	float speed=23.56;
+	double myspeed=3.1256;
+	bool idTrue=true;
+	string name="Lideya";
+	cout<<age<<endl;
+	cout<<speed<<endl;
+	cout<<myspeed<<endl;
+	cout<<idTrue<<endl;
+	cout<<name<<endl;
+	return 0;
+	
+	
+}
