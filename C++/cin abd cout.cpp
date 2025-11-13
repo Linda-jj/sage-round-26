b9091d27c3ab7e7f1d7c59c7298c70fb34a3793d
lideya <lideyafff@gmail.com>
2025-11-13 13:24:28 +0300

    Cin and cout

---
+++ C++/cin abd cout.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main (){
+	int grade1,grade2,sum;
+	cout<< "Enter your grade :"<<endl;
+	cin >> grade1;
+
+	cout<< "Enter your grade :"<<endl;
+	cin >> grade2;
+	
+	sum=grade1+grade2;
+	cout<<"Your Grade is : "<<sum<<endl;
+	return 0;
+	
+	
+}
