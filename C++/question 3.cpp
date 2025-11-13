f25c8fe494cd819a7cccaddb757efe8321a8e3af
lideya <lideyafff@gmail.com>
2025-11-13 14:01:12 +0300

    question 3

---
+++ C++/question 3.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+	const double pi= 3.14 ;
+	double radius,circumfrance;
+	cout<< "Enter radius "<<endl;
+	
+	circumfrance=2*pi*radius;
+	cout<<circumfrance<<endl;
+	return 0;
+	
+	
+}
