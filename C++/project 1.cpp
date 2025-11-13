176f1ea08c576460603060788130d5134c39c394
lideya <lideyafff@gmail.com>
2025-11-13 13:31:59 +0300

    project 0

---
+++ C++/project 1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+	int temp,frani;
+	cout<<"Enter the Temp :"<<endl;
+	cin>>temp;
+	frani=(temp*1.8)+32;
+	cout<< "The Temp is "<<frani;
+	
+	
+	
+}
