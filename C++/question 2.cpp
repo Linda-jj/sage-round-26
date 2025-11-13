9bf233945245352a3d9519ffe388ced37c1a6226
lideya <lideyafff@gmail.com>
2025-11-13 13:50:41 +0300

    question 2

---
+++ C++/question 2.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+	string name;
+	int age;
+	cout<<"Enter your Name : "<<endl;
+	cin>>name;
+	cout<<"Enter your age : "<<endl;
+	cin>>age;
+	
+	
+	cout<<"Your Name is "<<name <<" and Your age is"<< age;
+	return 0;
+	
+	
+}
