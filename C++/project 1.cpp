101ef405bd3d0678e19cde9b1f0c3fec5a065506
lideya <lideyafff@gmail.com>
2025-11-13 13:45:41 +0300

    Question 1

--- C++/project 1.cpp
+++ C++/project 1.cpp
@@ -1,7 +1,7 @@
 #include<iostream>
 using namespace std;
 int main(){
-	int temp,frani;
+	double temp,frani;
 	cout<<"Enter the Temp :"<<endl;
 	cin>>temp;
 	frani=(temp*1.8)+32;
---
+++ C++/question 1.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+	float num1,num2,result;
+	cout<<"Enter your Num 1 : "<<endl;
+	cin>>num1;
+	cout<<"Enter your Num 2 : "<<endl;
+	cin>>num2;
+	
+	result=num1*num2;
+	cout<<"Result is "<<result;
+	return 0;
+	
+	
+}
