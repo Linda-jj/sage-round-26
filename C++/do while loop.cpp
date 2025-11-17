8421fad8d7a3fe1651385727ff15a1051a64bb9b
merizmo <car>
2025-11-17 13:04:04 +0300

    di while loop

--- C++/do while loop.cpp
+++ C++/do while loop.cpp
@@ -2,9 +2,11 @@
 using namespace std;
 int main(){
 int i=1;
-while(i<=5){
+do{
 	cout<<i<<endl;
 	i++;
-}
+}while(i<=5);
+
+
 	return 0;
 }
--- C++/while loop even num print.cpp
+++ C++/while loop even num print.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 using namespace std;
 int main(){
-int i=1;
-while(i<=5){
-	cout<<i<<endl;
+int i=100;
+while(i<=0){
+	if(i%2==0){
+	cout<<i<<endl;	
+	}
 	i++;
 }
 	return 0;
