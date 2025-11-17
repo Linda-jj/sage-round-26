6e09a97d4ea66860b402e29c066da32d31c792c9
merizmo <car>
2025-11-17 12:07:30 +0300

    swicth condition

---
+++ C++/swicth condition.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main(){
+ int day=1;
+ switch(day){
+ 	case 1:cout<<"Monday"<<endl;
+	 break;	
+	case 2:cout<<"Tues"<<endl;
+	break;
+    case 3:cout<<"wend"<<endl;
+	break;
+    default: cout<<"No Day" ;
+   
+ }
+  return 0;	
+
+}
