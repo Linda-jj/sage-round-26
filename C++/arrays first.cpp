3698a0d93fde78d7b97e11338f415750f27db974
merizmo <car>
2025-11-18 12:09:43 +0300

    loop markes example

---
+++ C++/arrays first.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+using namespace std;
+int main(){
+	char vawol[]={'a','e','o','i','u'};
+	cout<<vawol[3];
+	
+	
+	return 0;
+	
+}
--- C++/for loop assigment.cpp
+++ C++/for loop assigment.cpp
@@ -1,15 +1,13 @@
 #include <iostream>
 using namespace std;
 int main(){
-for(int i=5;i<=5;i++){
+for(int i=5;i<=1;i--){
 	
-for( int j=1;j<=5-1;j++){
+for( int j=1;j<=i;j++){
 	
-	cout<<" ";
-}
-for(int x=1;x<=i;x++){
 	cout<<"*";
 }
+
 	
  	cout<<endl;
  }
