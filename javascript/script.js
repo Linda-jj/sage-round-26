c63893135c03da593e7dfcd2cbf028887509f0d1
merizmo <car>
2025-11-27 13:16:37 +0300

    conditinal fi and while

---
+++ javascript/script.js
@@ -0,0 +1,35 @@
+var x = 1;
+let y = 2;
+console.log(x + y);
+
+function sayHello(){
+    alert('Hello form function')
+}
+
+function sum(x,y){
+    return x+y;
+}
+
+console.log(sum(2,15));
+//sayHello();
+let age =18;
+
+if(age>=18){
+console.log('Adult');
+}
+else{
+    console.log('child');
+}
+
+
+let i=5;
+for(let i=1;i<=5;i++){
+    console.log(i);
+}
+
+let z=1;
+
+while( z<=5){
+    console.log(z);
+    z++;
+}
