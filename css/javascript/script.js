a1da181a7fdf86ea10e84e92cc4a9f9f2343e167
merizmo <car>
2025-11-27 13:04:15 +0300

    function on javascript

--- css/javascript/script.js
+++ css/javascript/script.js
@@ -1,3 +1,14 @@
 var x = 1;
 let y = 2;
 console.log(x + y);
+
+function sayHello(){
+    alert('Hello form function')
+}
+
+function sum(x,y){
+    return x+y;
+}
+
+console.log(sum(2,15));
+//sayHello();
