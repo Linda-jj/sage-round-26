a3070a06a6004a628085da4f32c5dfa4b4bf6813
lideya <lideyafff@gmail.com>
2025-12-27 22:14:32 +0300

    case studies finished

--- project/portfolio/src/App.js
+++ project/portfolio/src/App.js
@@ -1,11 +1,12 @@
 
 import './App.css';
 import Home from './componet/Home';
-
+import Case from './componet/Case';
 function App() {
   return (
     <div>
      <Home/>
+     <Case/>
     </div>
   );
 }
