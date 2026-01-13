b6a11955770b0eede323fae5621aa53de2aab0b2
lideya <lideyafff@gmail.com>
2026-01-14 00:21:32 +0300

    final project nav and header part

--- project/Final Project/hotel_booking/src/App.js
+++ project/Final Project/hotel_booking/src/App.js
@@ -1,10 +1,14 @@
 
 import './App.css';
+import Hearder from './components/Header/Hearder';
+import Nav_bar from './components/nav/Nav_bar';
 
 function App() {
   return (
     <div className="App">
       
+    <Nav_bar/>
+    <Hearder/>
     
     </div>
   );
