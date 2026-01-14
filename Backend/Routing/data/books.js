5af4f3b3d8ae3c135fd44d30dcc5261832f579a8
Lindajj <lideyafff@gmail.com>
2026-01-14 14:05:07 +0300

    backend routing get bruno

---
+++ Backend/Routing/data/books.js
@@ -0,0 +1,43 @@
+const books = [
+  {
+    id: 1,
+    title: "The Alchemist",
+    author: "Paulo Coelho",
+    genre: "Fiction",
+    price: 299,
+    publishedYear: 1988
+  },
+  {
+    id: 2,
+    title: "Atomic Habits",
+    author: "James Clear",
+    genre: "Self-help",
+    price: 450,
+    publishedYear: 2018
+  },
+  {
+    id: 3,
+    title: "Clean Code ",
+    author: "Robert C. Martin",
+    genre: "Programming",
+    price: 799,
+    publishedYear: 2008
+  },
+  {
+    id: 4,
+    title: "Rich Dad Poor Dad",
+    author: "Robert T. Kiyosaki",
+    genre: "Finance",
+    price: 350,
+    publishedYear: 1997
+  },
+      {id: 5,
+    title: "The Power of Habit",
+    author: "Charles Duhigg",
+    genre: "Psychology",
+    price: 499,
+    publishedYear: 2012
+  }
+];
+
+module.exports=books
---
+++ Backend/Routing/index.js
@@ -0,0 +1,50 @@
+const express= require('express')
+const books= require("./data/books")
+
+const app=express()
+ 
+app.get('/books',(req, res)=>{
+    res.status(200).json(books)
+});
+
+//query
+app.get("/books/search",(req,res)=>{
+    const q =req.query.q.toLowerCase()
+    const book =books.filter((book)=> book.title.toLowerCase().includes(q))
+    console.log(q)
+    res.status(200).json(book)
+})
+
+
+//params
+app.get('/books/:id',(req,res)=>{
+const id=req.params.id
+const book=books.find((book)=>book.id ==id)
+console.log(book)
+
+if (book == undefined){
+    res.status(404).json({
+        success:"false",
+        massgge:"book not found"
+
+    })
+}else{
+    res.status(200).json(book)
+}
+})
+// app.post("/",(req, res)=>{
+//     res.send("Home page from post method")
+// });
+// app.delete("/",(req, res)=>{
+//     res.send("Home page from delete method")
+// });
+// app.put("/",(req, res)=>{
+//     res.send("Home page from put method")
+// });
+
+
+
+
+app.listen(8000,()=>{
+    console.log("server is listening port ")
+})
